9e1f0a86364152597540af9c64e129538515ca5c
will worth <will.worth@gmail.com>
2019-12-26 14:50:22 +0100

    recipe model

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe} from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'test description', 'https://www.seriouseats.com/images/2016/04/20160410-menu-roasted-eggplant-lentil-vegan-5-thumb-1500xauto-430521.jpg')
+  ];
 
-  constructor() { }
+  constructor() { } 
 
   ngOnInit() {
   }
